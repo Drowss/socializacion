9ddfb4ff91722fc384fdab552c47925092262e91
Drow <esneiderhel@gmail.com>
2023-04-16 19:10:58 -0500

    social2

--- pqt2.py
+++ pqt2.py
@@ -35,15 +35,16 @@ class Registro_usuario(QMainWindow):
         #self.fondo.move(40,400)
         self.setCentralWidget(self.fondo)
 
-        self.user = QLabel("Usuario", self)
+        self.user = QLabel("Nombre y apellido", self)
+        self.user.setFixedWidth(140)
         self.user.setStyleSheet("background-color:transparent; color:#fff0b1;")
         self.user.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
         #self.user.move(265, 90)
-        self.user.move(260,85)
+        self.user.move(70,85)
 
         self.user1 = QLineEdit(self)
-        self.user1.setMaxLength(12)
-        self.user1.setPlaceholderText('Máximo 12 carácteres')
+        self.user1.setMaxLength(32)
+        self.user1.setPlaceholderText('Juan Camilo Sánchez')
         self.user1.setStyleSheet("""
                     QLineEdit {
                         background-color:#E0FFFF;
@@ -56,13 +57,13 @@ class Registro_usuario(QMainWindow):
                     }
                     """)
         self.user1.setFixedWidth(160)
-        self.user1.move(216, 120)
+        self.user1.move(60, 120)
 
         self.pwd = QLabel("Contraseña", self)
 
         self.pwd.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
         self.pwd.setStyleSheet("background-color:transparent;color: #fff0b1")
-        self.pwd.move(250, 175)
+        self.pwd.move(400, 85)
 
         self.pwd1 = QLineEdit(self)
         self.pwd1.setMaxLength(12)
@@ -80,13 +81,13 @@ class Registro_usuario(QMainWindow):
                     }
                     """)
         self.pwd1.setFixedWidth(160)
-        self.pwd1.move(216, 210)
+        self.pwd1.move(360, 120)
 
         self.email = QLabel("Correo electrónico", self)
         self.email.setFixedWidth(163)
         self.email.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
         self.email.setStyleSheet("background-color:transparent;color: #fff0b1")
-        self.email.move(225, 265)
+        self.email.move(60, 225)
 
         self.email1 = QLineEdit(self)
         self.email1.setPlaceholderText('Ejemplo123@gmail.com')
@@ -103,13 +104,13 @@ class Registro_usuario(QMainWindow):
                             }
                             """)
         self.email1.setFixedWidth(160)
-        self.email1.move(216, 300)
+        self.email1.move(60, 260)
         
         self.cc = QLabel("Cédula de Ciudadanía", self)
         self.cc.setFixedWidth(163)
         self.cc.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
         self.cc.setStyleSheet("background-color:transparent;color: #fff0b1")
-        self.cc.move(215, 365)
+        self.cc.move(360, 225)
 
         self.cc1 = QLineEdit(self)
         self.cc1.setMaxLength(12)
@@ -125,7 +126,51 @@ class Registro_usuario(QMainWindow):
                             }
                             """)
         self.cc1.setFixedWidth(160)
-        self.cc1.move(216, 400)
+        self.cc1.move(360, 260)
+
+        self.numero = QLabel("Número telefónico", self)
+        self.numero.setFixedWidth(163)
+        self.numero.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
+        self.numero.setStyleSheet("background-color:transparent;color: #fff0b1")
+        self.numero.move(65, 380)
+
+        self.numero1 = QLineEdit(self)
+        self.numero1.setMaxLength(12)
+        self.numero1.setStyleSheet("""
+                                    QLineEdit {
+                                        background-color:#E0FFFF;
+                                        border-radius: 10px;
+                                        border: 2px solid #e9f0e5;
+                                        padding: 5px;
+                                    }
+                                    QLineEdit:hover {
+                                        background-color:#B0C4DE;
+                                    }
+                                    """)
+        self.numero1.setFixedWidth(160)
+        self.numero1.move(60, 415)
+
+        self.Direccion = QLabel("Dirección residencia", self)
+        self.Direccion.setFixedWidth(163)
+        self.Direccion.setFont(QFont("Bahnschrift SemiLight SemiConde", 15))
+        self.Direccion.setStyleSheet("background-color:transparent;color: #fff0b1")
+        self.Direccion.move(360, 380)
+
+        self.Direccion1 = QLineEdit(self)
+        self.Direccion1.setMaxLength(80)
+        self.Direccion1.setStyleSheet("""
+                                    QLineEdit {
+                                        background-color:#E0FFFF;
+                                        border-radius: 10px;
+                                        border: 2px solid #e9f0e5;
+                                        padding: 5px;
+                                    }
+                                    QLineEdit:hover {
+                                        background-color:#B0C4DE;
+                                    }
+                                    """)
+        self.Direccion1.setFixedWidth(160)
+        self.Direccion1.move(360, 415)
 
         self.boton = QPushButton("Crear Cuenta", self)
         self.boton.setFont(QFont('Bahnschrift SemiLight SemiConde', 15))
